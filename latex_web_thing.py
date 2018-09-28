c138453d54e7332f98ded1ae81482e852ad25d13
Kevin Lui <kevinywlui@gmail.com>
2018-09-27 20:15:18 -0700

    main

--- latex_web_thing.py
+++ latex_web_thing.py
@@ -1,4 +1,5 @@
 import requests
+import sys
 
 
 def get_bibtex(**payload):
@@ -21,3 +22,27 @@ def parse_bib(filename):
             if line.startswith("@"):
                 all_entries.add(line.split('{', 1)[1].split(',', 1)[0])
     return all_entries
+
+
+def parse_aux(filename):
+    with open(filename, "r") as f:
+        citations = set()
+        for line in f:
+            if line.startswith("\\citation{"):
+                citations.add(line[10:-2])
+    return citations
+
+
+def main():
+    name = sys.argv[1]
+    aux_file = name + ".aux"
+    bib_file = name + ".bib"
+    citations = parse_aux(aux_file)
+    existing_bibs = parse_bib(bib_file)
+
+    missing = citations.difference(existing_bibs)
+    nikolas(missing, bib_file)
+
+
+if __name__ == "__main__":
+    main()

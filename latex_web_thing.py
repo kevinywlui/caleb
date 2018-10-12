bfc1d9919d03a18d869e419090b6bf8f1e0f0489
Kevin Lui <kevinywlui@gmail.com>
2018-10-11 19:14:04 -0700

    removed some nikolas stuff

--- latex_web_thing.py
+++ latex_web_thing.py
@@ -2,17 +2,18 @@ import requests
 import sys
 
 
-def get_bibtex(**payload):
-    r = requests.get('https://mathscinet.ams.org/mrlookup', params=payload)
-    output = r.text
-    return output.split('<pre>')[1].split('</pre>')[0].strip('\n')
-
-
 def parse_string(s):
     pieces = s.split(':')
     payload = dict(zip(pieces, ['au', 'ti', 'year']))
     payload['format'] = 'bibtex'
     return payload
+
+
+def get_bibtex(s):
+    payload = parse_string(s)
+    r = requests.get('https://mathscinet.ams.org/mrlookup', params=payload)
+    output = r.text
+    return output.split('<pre>')[1].split('</pre>')[0].strip('\n')
 
 
 def parse_bib(filename):
@@ -33,6 +34,13 @@ def parse_aux(filename):
     return citations
 
 
+def append_missing(missing_cits, bib_file):
+    with open(bib_file, "a") as f:
+        for m in missing_cits:
+            new_entry = get_bibtex(m)
+            f.write(new_entry)
+
+
 def main():
     name = sys.argv[1]
     aux_file = name + ".aux"
@@ -40,19 +48,10 @@ def main():
     citations = parse_aux(aux_file)
     existing_bibs = parse_bib(bib_file)
 
-    missing = citations.difference(existing_bibs)
-    nikolas(missing, bib_file)
+    missing_cits = citations.difference(existing_bibs)
+    if missing_cits:
+        append_missing(missing_cits, bib_file)
 
 
 if __name__ == "__main__":
     main()
-def add_to_bibfile(citations,bib_file):
-
-	with  open(bib_file,"a+") as f:
-
-	for citation in citations:
-
-		f.write(get_bibtex(parse_string(citation))
-
-
-

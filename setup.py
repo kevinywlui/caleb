5f57a94e4c10fc723dcebd2a4367ff08e1e4398b
kevin lui <kevinywlui@gmail.com>
2019-07-14 15:29:13 -0700

    Set up setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="Caleb",
+    description="A tool automatically to fill in your Latex citations using AMS Lookup.",
+    author="Kevin Lui",
+    author_email="kevinywlui@gmail.com",
+    scripts=["bin/caleb"],
+    version="1.0",
+    url="https://github.com/kevinywlui/caleb",
+)

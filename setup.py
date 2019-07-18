72baa9acf6da035832631d3e3ba3012f09570452
kevin lui <kevinywlui@gmail.com>
2019-07-18 15:54:03 -0700

    Specify python3 in the caleb script

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     author="Kevin Lui",
     author_email="kevinywlui@gmail.com",
     scripts=["bin/caleb"],
-    version="0.2.3",
+    version="0.2.4",
     url="https://github.com/kevinywlui/caleb",
     packages=setuptools.find_packages(),
     classifiers=[

b8ee6ed41cc6975df6776e66870bbeec0f69d23b
kevin lui <kevinywlui@gmail.com>
2019-07-23 15:53:12 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,15 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="caleb",
-    description="A tool automatically to fill in your Latex citations using AMS Lookup.",
+    description="A tool to automatically retrieve bibtex entries",
     author="Kevin Lui",
     author_email="kevinywlui@gmail.com",
     scripts=["bin/caleb"],
-    version="0.4.1",
+    version="0.4.2",
     url="https://github.com/kevinywlui/caleb",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Topic :: Text Processing :: Markup :: LaTeX",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",

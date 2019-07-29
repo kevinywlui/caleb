9bcf05fef8d8b7674ccae9b3b8a795287873c587
kevin lui <kevinywlui@gmail.com>
2019-07-28 23:54:47 -0700

    WIP

---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+import pytest
+
+@pytest.fixture()
+def caleb_on
--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -1,5 +1,10 @@
 import pytest
+from shutil import copyfile
 
 
-def test_clean_tex():
-    
+# Here we test
+def test_clean_tex(tmp_path):
+    """Test a tex file with an empty bib without the --take-first flag.
+    """
+    # Copy over files to tmp_path
+    copyfile(

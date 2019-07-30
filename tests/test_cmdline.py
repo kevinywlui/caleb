1ca3f999e49a17729374ff3b9d496bb830d8777b
kevin lui <kevinywlui@gmail.com>
2019-07-29 20:47:02 -0700

    Add a test for a missing input name

--- tests/test_cmdline.py
+++ tests/test_cmdline.py
@@ -5,3 +5,8 @@ from caleb.__version__ import __version__
 def test_version_request():
     version = subprocess.check_output(["poetry", "run", "caleb", "--version"])
     assert version.decode("utf-8")[:-1] == __version__
+
+
+def test_missing_argument():
+    error = subprocess.check_output(["poetry", "run", "caleb"])
+    assert error.decode("utf-8")[:-1] == "Need input name"

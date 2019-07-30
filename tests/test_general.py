ac67d42a19899ab0a3b5048c59a07ab4b66f53e6
kevin lui <kevinywlui@gmail.com>
2019-07-29 20:37:07 -0700

    Add a test case where multiple results appear

--- tests/test_general.py
+++ tests/test_general.py
@@ -3,7 +3,6 @@ from caleb.file_handler import AuxHandler
 from shutil import copy
 from pathlib import Path
 
-
 tex_dir = Path("tests/tex_files")
 control_bib = tex_dir / "control_bibs" / "test_control.bib"
 
@@ -27,6 +26,25 @@ def test_clean_take_all(tmp_path):
     # Run caleb
     app = Application(str(test_file), verbose_level=0)
     app.go(take_first=True)
+    test_bib = (tmp_path / AuxHandler(str(test_file)).bibdata()).with_suffix(".bib")
+
+    # Compare with control
+    assert cmp_files(test_bib, control_bib)
+
+
+def test_dirty_not_take_all(tmp_path):
+    """Test a tex file with an empty bib without the --take-first flag.
+    """
+    test_file = tex_dir / "test.aux"
+    partial_file = tex_dir / "biblio.bib"
+
+    # Copy to tmp_path and work from there
+    test_file = copy(test_file, tmp_path)
+    copy(partial_file, tmp_path)
+
+    # Run caleb
+    app = Application(str(test_file), verbose_level=0)
+    app.go(take_first=False)
     test_bib = (tmp_path / AuxHandler(str(test_file)).bibdata()).with_suffix(".bib")
 
     # Compare with control

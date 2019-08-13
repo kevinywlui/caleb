7699519d6b80e81221978cf2e9ed61ede1f45784
kevin lui <kevinywlui@gmail.com>
2019-08-13 16:00:16 -0700

    Add tests for ams references

--- tests/test_reference.py
+++ tests/test_reference.py
@@ -9,6 +9,7 @@ from .consts import mazur_eisenstein_citation
 
 
 def test_when_exists_and_is_unique():
+    # the crossref version yields a heisenbug
     ref = Reference("mazur:eisenstein", method="ams")
     assert ref.exists()
     assert ref.is_unique()
@@ -16,19 +17,33 @@ def test_when_exists_and_is_unique():
 
 
 def test_when_exists_and_not_is_unique():
-    ref = Reference("mazur:modular")
+    ref = Reference("mazur:modular", method="crossref")
+    assert ref.exists()
+    assert not ref.is_unique()
+
+    ref = Reference("mazur:modular", method="ams")
     assert ref.exists()
     assert not ref.is_unique()
 
 
 def test_when_exists_and_is_unique_with_year():
-    ref = Reference("mazur:modular:2000")
+    ref = Reference("mazur:modular:2000", method="crossref")
+    assert ref.exists()
+    assert ref.is_unique()
+
+    ref = Reference("mazur:modular:2000", method="ams")
     assert ref.exists()
     assert ref.is_unique()
 
 
 def test_when_not_exists_and_not_is_unique():
-    ref = Reference("fermat:marvelous")
+    ref = Reference("fermat:marvelous", method="crossref")
+    assert not ref.exists()
+    with pytest.raises(ValueError):
+        ref.is_unique()
+    assert ref.bibtex() == ""
+
+    ref = Reference("fermat:marvelous", method="ams")
     assert not ref.exists()
     with pytest.raises(ValueError):
         ref.is_unique()
